ee020452c313e76f26bc23381d8425cd23b08c25
Pau Fernández <pau.fernandez@upc.edu>
2024-10-16 14:39:02 +0200

    Removed files for driver std
    They should have been a submodule

--- tests/hello-world/jutge-driver-std/std/etc/py/runpython_wrapper.py
+++
@@ -1,9 +0,0 @@
-# Python wrapper used to run the submission
-
-import os, sys, signal
-
-try:
-    sys.path = ['subdir'] + sys.path
-    import work
-except:
-    os.kill(os.getpid(), signal.SIGUSR2)
