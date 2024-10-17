68bf2d64c190d4e736efd6e33e8e7036eac27a97
Pau Fernández <pau.fernandez@upc.edu>
2024-10-17 10:41:17 +0200

    Adjusted Jest config for "non-monster" computers

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
+import { cpus } from "node:os"
+
+const numCPUs = cpus().length
+
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-	/* NOTE(pauek): These two lines are extremely important for now!
-	      Concurrency in the tests produces errors :( */
-	maxConcurrency: 16,
-	maxWorkers: 16,
+	maxConcurrency: 1,
 
 	testTimeout: 20000, // FIXME(pauek): Go needs more than 5000 for now...
 
