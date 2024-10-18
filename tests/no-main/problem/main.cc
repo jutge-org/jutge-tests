29857c1525af7eb18cf614bca9e86994007b7dc2
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2024-10-18 11:53:56 +0200

    Adjust original sample and problem and add Python3 no-main test

--- tests/no-main/problem/main.cc
+++ tests/no-main/problem/main.cc
@@ -1,10 +1,11 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     int x, y;
     cin >> x >> y;
+    cout.setf(ios::fixed);
+    cout.precision(1);
     double d = average(x, y);
     cout << d << endl;
 }
---
+++ tests/no-main/problem/main.py
@@ -0,0 +1,6 @@
+from yogi import read
+
+x = read(int)
+y = read(int)
+a = average(x, y)
+print(f"{a:.1f}")
